4b2da1e20233f8b51bfd7b34489f652c2223bb72
Christopher Kiernan <C16Christopher.Kiernan@usafa.edu>
2014-12-08 10:59:00 -0700

    Added/modified comments to code

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 // File:	Lab 7
 // Date:	Started 3 Dec 14
 // Purp:	Collect samples from P1.3-P1.5 in order
+// Note:	I use polling in this system to read the values from all
+//			of the sensors, which is less efficient. However, for the
+//			speed of our clock, the difference is not significant
 //-----------------------------------------------------------------
 #include "msp430g2553.h"
 
@@ -12,10 +15,10 @@ void initMSP430();
 //----------------------------------------------------------------------
 int main(void) {
 
-	unsigned short CTRsample[16];								// Just to analyze the values
+	unsigned short CTRsample[16];								// Arrays to hold the values of the ADC conversions
 	unsigned short RGTsample[16];
 	unsigned short LFTsample[16];
-	unsigned char i = 0;										// index into sample array
+	unsigned char i = 0;										// indexes into sample arrays
 	unsigned char j = 0;
 	unsigned char k = 0;
 
@@ -26,57 +29,62 @@ int main(void) {
 	DCOCTL = CALDCO_8MHZ;
 
 	P1DIR |= BIT0;												// Set the red LED as output
-	P1DIR |= BIT6;
+	P1DIR |= BIT6;												// Set the green LED as output
 
 	while(1) {
 
 		// LEFT SENSOR READ
-		ADC10CTL0 = 0;
-		ADC10CTL1 = INCH_3 | ADC10DIV_3;
-		ADC10AE0 = BIT3;
+		//--------------------
+		ADC10CTL0 = 0;											// Turn off ADC to make changes
+		ADC10CTL1 = INCH_3 | ADC10DIV_3;						// Use channel A3, CLK/4
+		ADC10AE0 = BIT3;										// Enable P1.3 as input
 		ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;		// Vcc & Vss as reference
 
-		ADC10CTL0 |= ADC10SC;
+		ADC10CTL0 |= ADC10SC;									// ADC Conversion
 		while(ADC10CTL1 & ADC10BUSY);
-		LFTsample[i] = ADC10MEM;
-		if (LFTsample[i] > 0x02B0){
+		LFTsample[i] = ADC10MEM;								// Collect value from ADC
+		if (LFTsample[i] > 0x02B0){								// Compare to threshold value to turn on LED
 			P1OUT |= BIT0;
 		}
 
 
 		// CENTER SENSOR READ
-		ADC10CTL0 = 0;											// Turn off ADC subsystem
-		ADC10CTL1 = INCH_4 | ADC10DIV_3 ;						// Channel 4, ADC10CLK/4
-		ADC10AE0 = BIT4;		 								// Make P1.4 analog input
+		//----------------------
+		ADC10CTL0 = 0;											// Turn off ADC to make changes
+		ADC10CTL1 = INCH_4 | ADC10DIV_3 ;						// Use channel A4, CLK/4
+		ADC10AE0 = BIT4;		 								// Enable P1.4 as input
 		ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;		// Vcc & Vss as reference
 
-		ADC10CTL0 |= ADC10SC;									// Start a conversion
+		ADC10CTL0 |= ADC10SC;									// ADC Conversion
 		while(ADC10CTL1 & ADC10BUSY);							// Wait for conversion to complete
-		CTRsample[j] = ADC10MEM;									// collect that 10-bit value
-		if (CTRsample[j] > 0x02B0){
+		CTRsample[j] = ADC10MEM;								// Collect value from ADC
+		if (CTRsample[j] > 0x02B0){								// Compare to threshold value to turn on LED
 			P1OUT |= BIT0;
 			P1OUT |= BIT6;
 		}
 
 		// RIGHT SENSOR READ
-		ADC10CTL0 = 0;
-		ADC10CTL1 = INCH_5 | ADC10DIV_3;
-		ADC10AE0 = BIT5;
+		//---------------------
+		ADC10CTL0 = 0;											// Turn off ADC to make changes
+		ADC10CTL1 = INCH_5 | ADC10DIV_3;						// Use channel A5, CLK/4
+		ADC10AE0 = BIT5;										// Enable P1.5 as ADC input
 		ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;		// Vcc & Vss as reference
 
-		ADC10CTL0 |= ADC10SC;
+		ADC10CTL0 |= ADC10SC;									// ADC conversion
 		while(ADC10CTL1 & ADC10BUSY);
 		RGTsample[k] = ADC10MEM;
-		if (RGTsample[k] > 0x02B0){
+		if (RGTsample[k] > 0x02B0){								// Compare ADC value to threshold to turn on LED
 			P1OUT |= BIT6;
 		}
 
+																// Below turns off the LEDs if there is no sensor reading
 		if (LFTsample[i]<0x02B0 && CTRsample[j]<0x02B0 && RGTsample[k]<0x02B0){
 			P1OUT &= ~BIT0;
 			P1OUT &= ~BIT6;
 		}
+
 		i = (i+1) & 0xF;										// This is a mod 16 increment
-		i=0;
+		i=0;													// Reset the array values
 		j=0;
 		k=0;
 	} // end infinite loop
